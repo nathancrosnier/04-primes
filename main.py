120e63e8bcd632027534c94690f53036e2ba98cf
nathancrosnier <nathan.crosnier@edu.esiee.fr>
2024-11-07 08:33:57 +0000

    Exercice 4 : prime

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
+"""
+Un code pour tester si des nombres sont premier
+"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
-
-    # votre code ici
-
-    pass
+    """
+    Retourne un booléen indiquant si le nombre passé en paramètre est premier.
+    """
+    if p<= 1:
+        return False
+    n = int(sqrt(p))
+    for i in range(2, n+1):
+        if p%i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    Tests de fonction isprime()
+    """
     # vos appels à la fonction secondaire ici
-
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
 
 
